1f8727f82f6782aa5ebe1129d800558a0df362b2
taichiyi <taichiyi@foxmail.com>
2018-11-16 20:06:15 +0800

    ci: 1.0.0-rc.1

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -8,7 +8,7 @@ export const OSS_IMAGE_QUALITY = 70; // 图片压缩比例；1-100；100：不�
 export const LINE_HEIGHT_RATIO = 1.618; // 行高
 export const IS_PROMPT_FIRSTPAY = true; // 没有选择首付时是否提示选择首付
 export const MIN_FIRSTPAY_RATE = 0.1; // 最低首付比: 0, 0.1, 0.2, 0.3, 0.4, 0.5
-export const VERSION = '1.0.0-alpha.1';
+export const VERSION = '1.0.0-rc.1';
 
 // namespace -start-
 export const GETADVERSTTOPINFO_NAMESPACE = 'getAdverstTopInfo';
