71d48f0f74c9e9dd8f2185850dbb26f897abf4f4
taichiyi <taichiyi@foxmail.com>
2018-11-19 16:48:20 +0800

    fix: 默认多语言问题

--- src/global.js
+++ src/global.js
@@ -1,4 +1,10 @@
 // import '@babel/polyfill';
+import { setLocale } from 'umi/locale';
+import { IS_I18N } from './common/constants';
 
 // global.Intl = require('intl');
 // window.Intl = require('intl');
+
+setInterval(() => {
+  setLocale(IS_I18N ? 'zh-CN' : 'vi-VN');
+}, 300);
