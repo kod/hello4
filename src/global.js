0481546ef3a18827236e3401101a8d0496ca167d
taichiyi <taichiyi@foxmail.com>
2018-11-21 14:07:40 +0800

    refactor: 注释无用代码

--- src/global.js
+++ src/global.js
@@ -1,10 +1,10 @@
 // import '@babel/polyfill';
-import { setLocale } from 'umi/locale';
-import { IS_I18N } from './common/constants';
 
 // global.Intl = require('intl');
 // window.Intl = require('intl');
 
-setInterval(() => {
-  setLocale(IS_I18N ? 'zh-CN' : 'vi-VN');
-}, 300);
+// import { setLocale } from 'umi/locale';
+// import { IS_I18N } from './common/constants';
+// setTimeout(() => {
+//   setLocale(true ? 'zh-CN' : 'vi-VN');
+// }, 300);
