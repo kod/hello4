905f4d4b542b470a7abcc564b1862905d5ead216
taichiyi <taichiyi@foxmail.com>
2018-11-21 14:45:59 +0800

    refactor(global.js): 设置默认语言

--- src/global.js
+++ src/global.js
@@ -4,7 +4,8 @@
 // window.Intl = require('intl');
 
 // import { setLocale } from 'umi/locale';
-// import { IS_I18N } from './common/constants';
+import { IS_I18N } from './common/constants';
 // setTimeout(() => {
 //   setLocale(true ? 'zh-CN' : 'vi-VN');
 // }, 300);
+localStorage.setItem('umi_locale', IS_I18N ? 'zh-CN' : 'vi-VN');
