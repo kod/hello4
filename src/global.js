d1b555b6586e6556a7aedeee88aeb5a7248a9877
taichiyi <taichiyi@foxmail.com>
2018-11-23 10:55:30 +0800

    fix(global.js): 切换多语言没变化

--- src/global.js
+++ src/global.js
@@ -8,4 +8,9 @@ import { IS_I18N } from './common/constants';
 // setTimeout(() => {
 //   setLocale(true ? 'zh-CN' : 'vi-VN');
 // }, 300);
-localStorage.setItem('umi_locale', IS_I18N ? 'zh-CN' : 'vi-VN');
+
+const isInitUmiLocale = localStorage.getItem('isInitUmiLocale');
+if (isInitUmiLocale !== 'true') {
+  localStorage.setItem('umi_locale', IS_I18N ? 'zh-CN' : 'vi-VN');
+  localStorage.setItem('isInitUmiLocale', 'true');
+}
